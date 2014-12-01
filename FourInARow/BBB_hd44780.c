95470fdcbfac3e5622425001e9c16567755f2bc1
Matt Cooley <matt@mattcooley.net>
2014-12-01 13:56:57 -0600

    Updated LCD code with comments and backlight driver

--- FourInARow/BBB_hd44780.c
+++ FourInARow/BBB_hd44780.c
@@ -51,7 +51,14 @@
 #define LCD_5x10DOTS 0x04
 #define LCD_5x8DOTS  0x00
 
-void pulse_lcd()
+// Backlight PWM
+#define LCD_BACKLIGHT_PWM_HZ 100.0f
+
+/**
+ * Send a pulse on the E(nable) line so the LCD latches whatever value is
+ * currently on the data lines.
+ */
+void lcd_pulse()
 {
 	struct timespec pulse_length;
 	pulse_length.tv_sec = 0;
@@ -65,7 +72,12 @@ void pulse_lcd()
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &pulse_length, NULL);
 }
 
-void write_lcd(char command, int mode)
+/**
+ * Write the given byte to the LCD. Mode can be either LCD_MODE_COMMAND for
+ * writing to the instruction register or LCD_MODE_CHARACTER for writing to
+ * the data register.
+ */
+void lcd_write(uint8_t command, int mode)
 {
 	struct timespec command_execution_wait;
 	command_execution_wait.tv_sec = 0;
@@ -86,7 +98,7 @@ void write_lcd(char command, int mode)
 	(((command >> 5) & 1) > 0) ? pin_low(LCD_DB5_PIN) : pin_high(LCD_DB5_PIN);
 	(((command >> 6) & 1) > 0) ? pin_low(LCD_DB6_PIN) : pin_high(LCD_DB6_PIN);
 	(((command >> 7) & 1) > 0) ? pin_low(LCD_DB7_PIN) : pin_high(LCD_DB7_PIN);
-	pulse_lcd();
+	lcd_pulse();
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &command_execution_wait, NULL);
 
 	// Send the lower 4 bits
@@ -94,11 +106,33 @@ void write_lcd(char command, int mode)
 	(((command >> 1) & 1) > 0) ? pin_low(LCD_DB5_PIN) : pin_high(LCD_DB5_PIN);
 	(((command >> 2) & 1) > 0) ? pin_low(LCD_DB6_PIN) : pin_high(LCD_DB6_PIN);
 	(((command >> 3) & 1) > 0) ? pin_low(LCD_DB7_PIN) : pin_high(LCD_DB7_PIN);
-	pulse_lcd();
+	lcd_pulse();
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &command_execution_wait, NULL);
 }
 
-void initialize_lcd()
+/**
+ * Move the cursor to the specified column and row. Column and row indexes
+ * start at 0. The user must ensure that the column and row are valid for
+ * the LCD that is attached. In any case, no more than 4 rows are supported.
+ */
+void lcd_move_cursor(unsigned int column, unsigned int row)
+{
+	unsigned int offset = 0x00;
+	switch (row)
+	{
+	case 0:
+		offset = 0x00; break;
+	case 1:
+		offset = 0x40; break;
+	case 2:
+		offset = 0x14; break;
+	case 3:
+		offset = 0x54; break;
+	}
+	lcd_write(LCD_SETDDRAMADDR | (column + offset), LCD_MODE_COMMAND);
+}
+
+void lcd_initialize()
 {
 	// Enable GPIO devices
 	BBBIO_sys_Enable_GPIO(0);
@@ -113,46 +147,32 @@ void initialize_lcd()
 	iolib_setdir(LCD_DB6_PIN, BBBIO_DIR_OUT);
 	iolib_setdir(LCD_DB7_PIN, BBBIO_DIR_OUT);
 
+	// Turn off backlight
+	lcd_set_backlight(0, 0, 0);
+
 	// Prepare for 4-bit operation
-	write_lcd(0x33, LCD_MODE_COMMAND);
-	write_lcd(0x32, LCD_MODE_COMMAND);
+	lcd_write(0x33, LCD_MODE_COMMAND);
+	lcd_write(0x32, LCD_MODE_COMMAND);
 
     // Initialize registers
-	write_lcd(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF, LCD_MODE_COMMAND);
-	write_lcd(LCD_FUNCTIONSET | LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS, LCD_MODE_COMMAND);
-	write_lcd(LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT, LCD_MODE_COMMAND);
+	lcd_write(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF, LCD_MODE_COMMAND);
+	lcd_write(LCD_FUNCTIONSET | LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS, LCD_MODE_COMMAND);
+	lcd_write(LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT, LCD_MODE_COMMAND);
 
-	clear_lcd();
+	lcd_clear();
 }
 
-void clear_lcd()
+void lcd_clear()
 {
 	struct timespec command_execution_wait;
 	command_execution_wait.tv_sec = 0;
 	command_execution_wait.tv_nsec = 3000000; // 3ms. This command takes a long time.
 
-	write_lcd(LCD_CLEARDISPLAY, LCD_MODE_COMMAND);
+	lcd_write(LCD_CLEARDISPLAY, LCD_MODE_COMMAND);
 	clock_nanosleep(CLOCK_MONOTONIC, 0, &command_execution_wait, NULL);
 }
 
-void move_cursor(unsigned int column, unsigned int row)
-{
-	unsigned int offset = 0x00;
-	switch (row)
-	{
-	case 0:
-		offset = 0x00; break;
-	case 1:
-		offset = 0x40; break;
-	case 2:
-		offset = 0x14; break;
-	case 3:
-		offset = 0x54; break;
-	}
-	write_lcd(LCD_SETDDRAMADDR | (column + offset), LCD_MODE_COMMAND);
-}
-
-void write_string(char string[])
+void lcd_write_string(char string[])
 {
     int index = 0;
     int currentRow = 0;
@@ -162,12 +182,42 @@ void write_string(char string[])
     	if (current == '\n')
     	{
     		++currentRow;
-    		move_cursor(0, currentRow);
+    		lcd_move_cursor(0, currentRow);
     	}
     	else
     	{
-    		write_lcd(current, LCD_MODE_CHARACTER);
+    		lcd_write(current, LCD_MODE_CHARACTER);
     	}
     	++index;
     }
+}
+
+void lcd_set_backlight(uint8_t red, uint8_t green, uint8_t blue)
+{
+	float redDuty = 100.0 * red / 255.0;
+	float greenDuty = 100.0 * green / 255.0;
+	float blueDuty = 100.0 * blue / 255.0;
+
+	if (redDuty < 0.01 && greenDuty < 0.01)
+	{
+		BBBIO_ehrPWM_Disable(BBBIO_PWMSS1);
+	}
+	else
+	{
+		// The PWM misbehaves when the duty cycle is exactly 0.
+		if (redDuty < 0.01) redDuty = 0.01;
+		if (greenDuty < 0.01) greenDuty = 0.01;
+
+		BBBIO_PWMSS_Setting(BBBIO_PWMSS1, LCD_BACKLIGHT_PWM_HZ, redDuty, greenDuty);
+		BBBIO_ehrPWM_Enable(BBBIO_PWMSS1);
+	}
+	if (blueDuty < 0.01)
+	{
+		BBBIO_ehrPWM_Disable(BBBIO_PWMSS2);
+	}
+	else
+	{
+		BBBIO_PWMSS_Setting(BBBIO_PWMSS2, LCD_BACKLIGHT_PWM_HZ, 0.0, blueDuty);
+		BBBIO_ehrPWM_Enable(BBBIO_PWMSS2);
+	}
 }
--- FourInARow/BBB_hd44780.h
+++ FourInARow/BBB_hd44780.h
@@ -8,6 +8,8 @@
 #ifndef BBB_HD44780_H_
 #define BBB_HD44780_H_
 
+#include <stdint.h>
+
 // Pins for LCD
 // Note: assumes inverting level shifters are used
 #define LCD_RS_PIN  8,8
@@ -17,8 +19,32 @@
 #define LCD_DB6_PIN 8,14
 #define LCD_DB7_PIN 8,12
 
-void initialize_lcd();
-void write_string(char string[]);
-void clear_lcd();
+// Pins for backlight:
+// Red:   EHRPWM1A (9,14)
+// Green: EHRPWM1B (9,16)
+// Blue:  EHRPWM2B (8,13)
+
+/**
+ * Prepare peripherals to communicate with the LCD and clear the screen.
+ */
+void lcd_initialize();
+
+/**
+ * Write a string to the LCD. The newline character can be used to print
+ * multi-line messages. The user must ensure that the string fits within
+ * the LCD boundary (likely 16x2 characters).
+ */
+void lcd_write_string(char string[]);
+
+/**
+ * Clears the LCD screen.
+ */
+void lcd_clear();
+
+/**
+ * Sets the color of the LCD backlight. Red, green, and blue intensities range
+ * from 0 to 255.
+ */
+void lcd_set_backlight(uint8_t red, uint8_t green, uint8_t blue);
 
 #endif /* BBB_HD44780_H_ */
